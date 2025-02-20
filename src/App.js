2d4059563d280f9a463d375f978a5d1f42e4a482
Barbara Żukowska <zukowska.barb@gmail.com>
2025-02-20 22:09:34 +0100

    'Back' button added, cards display adjusted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import products from "./db.json";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useLocation, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
@@ -8,7 +8,7 @@ import styles from "./App.module.css"
 function App() {
   const [productsList, setProductsList] = useState([]);
   const [loading, setLoading] = useState(true);  
-  
+
   useEffect(() => {
     setTimeout(() => {  
       setProductsList(products);
@@ -48,14 +48,42 @@ function App() {
       return prevCart; 
     });
   };
+
+  const Header = () => {
+    
+    const location = useLocation();
+    const navigate = useNavigate();
+    const isHome = location.pathname === "/";
+    
+    if(loading) return null;
+
+    if(isHome) return (
+      <header className={styles["App-header-style-homepage"]} >
+        <div className={styles['App-header-cart']}>
+          <Link to="/cart" >🛒✨ ({cart.length})</Link>
+        </div>
+        
+      </header>
+    );
+    
+    return (
+        <header className={styles["App-header-style"]} >
+          {!isHome && (
+            <span className={styles["App-header-back-button"]} onClick={() => navigate(-1)}>
+              ← Back
+            </span>
+          )}
+          <div className={styles['App-header-cart']}>
+            <Link to="/cart" >🛒✨ ({cart.length})</Link>
+          </div>
+          
+        </header>
+      )
+  };
   return (
     <div className={styles['App-container']}>
       <BrowserRouter basename="/shop-site-react-practice">
-        {!loading && (
-          <header className={styles['App-header-cart']}>
-            <Link to="/cart">🛒✨ ({cart.length})</Link>
-          </header>
-        )}
+        <Header />
         <Routes>
           <Route path="/" element={loading ? <div className={styles['App-container-loading']}><h2>Loading...</h2></div> : <Home productsList={productsList} addToCart={addToCart} />}/>
           <Route path="/product/:id" element={loading ? <div className={styles['App-container-loading']}><h2>Loading...</h2></div> : <ProductDetails productsList={productsList} addToCart={addToCart} />}/>
--- src/ProductCard.js
+++ src/ProductCard.js
@@ -9,10 +9,10 @@ function ProductCard({product, addToCart}) {
                 <img src={product.fotoSrc} alt={product.name} />
             </div>
             <div className={styles['card-text-container']}>
-                <h3 className={styles['product-card-title']}>{product.name}</h3>
-                <p className={styles["product-card-price"]}>{product.price}€</p>
+                <h3 className={styles['product-card-title']}>{product.name}</h3> 
             </div>
             <div className={styles['card-button-container']}>
+            <p className={styles["product-card-price"]}>{product.price}€</p>
               <button className={`${styles['product-card-button']} ${styles['product-card-button--small']}`} onClick={() => addToCart(product)}>Add to cart</button>
             </div>
           </div>
