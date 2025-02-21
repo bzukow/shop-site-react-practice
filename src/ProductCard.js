30f7f9321c4740ef8a4c028de8d4721a4e0e61fc
Barbara Żukowska <zukowska.barb@gmail.com>
2025-02-21 14:46:43 +0100

    Buttons adjustments

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -3,20 +3,21 @@ import { Link } from "react-router-dom";
 
 function ProductCard({product, addToCart}) {
     return (
-        <Link to={`/product/${product.id}`}>
+        
           <div className={styles['product-card']}>
-            <div className={styles['card-image-container']}>
-                <img src={product.fotoSrc} alt={product.name} />
-            </div>
-            <div className={styles['card-text-container']}>
-                <h3 className={styles['product-card-title']}>{product.name}</h3> 
-            </div>
+            <Link to={`/product/${product.id}`}>
+              <div className={styles['card-image-container']}>
+                  <img src={product.fotoSrc} alt={product.name} />
+              </div>
+              <div className={styles['card-text-container']}>
+                  <h3 className={styles['product-card-title']}>{product.name}</h3> 
+              </div>
+            </Link>
             <div className={styles['card-button-container']}>
             <p className={styles["product-card-price"]}>{product.price}€</p>
               <button className={`${styles['product-card-button']} ${styles['product-card-button--small']}`} onClick={() => addToCart(product)}>Add to cart</button>
             </div>
           </div>
-        </Link>
      
     );
   }
